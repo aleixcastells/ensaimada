12e645d66c3323865201fde10a70ffc1907de985
aleixcastells <castells.94@gmail.com>
2023-01-05 04:03:47 +0100

    Functionality improvements
    Added color changing buttons to indicate selected settings.
    
    Also started developing responsive recipe.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
 var clicks = 0
 
+var activeBtn = ''
+
+var settings = ['TamanyNormal', 'OusNormals', 'TempsFred']
+
 var image1 = document.getElementById('image1')
 var image2 = document.getElementById('image2')
 var image3 = document.getElementById('image3')
+
+// Ingredients petita, ous normals
+var igSucre = 150
+var igOus = 2
+var igFarina = 500
+var igAigua = 160
+var igLlevadura = 20
+
+var tamanyMult = 1
+var ousMult = 1
+var fredMult = 1
 
 
 function nextImage() {
@@ -29,9 +44,7 @@ function nextImage() {
             image2.classList.remove('active')
             break;
     }
-    console.log(clicks)
 }
-
 
 function prevImage() {
 
@@ -58,5 +71,104 @@ function prevImage() {
             image1.classList.remove('active')
             break;
     }
-    console.log(clicks)
 }
+
+function btnClick(n) {
+
+    activeBtn = n
+
+    btnAction(n)
+    document.getElementById(n).classList.replace("btn-secondary", "btn-primary")
+
+    quantityUpdate()
+    aiguaCalc()
+    consoleLog()
+}
+
+function consoleLog() {
+
+    console.clear()
+    console.log(settings)
+    console.log(aiguaCalc())
+}
+
+function btnAction(n) {
+
+    let tn = document.getElementById('TamanyNormal')
+    let tg = document.getElementById('TamanyGrossa')
+    let op = document.getElementById('OusPetits')
+    let on = document.getElementById('OusNormals')
+    let og = document.getElementById('OusGrossos')
+    let tf = document.getElementById('TempsFred')
+    let tc = document.getElementById('TempsCalor')
+
+    if (activeBtn == 'TamanyNormal' || activeBtn == 'TamanyGrossa') {
+
+        tn.classList.replace("btn-primary", "btn-secondary")
+        tg.classList.replace("btn-primary", "btn-secondary")
+
+        settings[0] = n
+    }
+
+    if (activeBtn == 'OusPetits' || activeBtn == 'OusNormals' || activeBtn == 'OusGrossos') {
+
+        op.classList.replace("btn-primary", "btn-secondary")
+        on.classList.replace("btn-primary", "btn-secondary")
+        og.classList.replace("btn-primary", "btn-secondary")
+
+        settings[1] = n
+    }
+
+    if (activeBtn == 'TempsFred' || activeBtn == 'TempsCalor') {
+
+        tf.classList.replace("btn-primary", "btn-secondary")
+        tc.classList.replace("btn-primary", "btn-secondary")
+
+        settings[2] = n
+    }
+}
+
+
+function quantityUpdate() {
+
+    let totalOus = Math.round(ratioCalc(igOus) * ousCalc())
+
+    document.getElementById('sucre').innerHTML = ratioCalc(igSucre) + ' g'
+    document.getElementById('ous').innerHTML = totalOus
+    document.getElementById('farina').innerHTML = ratioCalc(igFarina) + ' g'
+    document.getElementById('aigua').innerHTML = aiguaCalc() + ' ml'
+    document.getElementById('llevadura').innerHTML = ratioCalc(igLlevadura) + ' g'
+}
+
+function ratioCalc(ig) {
+
+    if (settings[0] == 'TamanyNormal') { tamanyMult = 1 }
+    if (settings[0] == 'TamanyGrossa') { tamanyMult = 1.50 }
+
+    let ratio = ig * (tamanyMult)
+
+    return ratio
+
+}
+
+function ousCalc() {
+
+    switch (settings[1]) {
+
+        case 'OusPetits':
+            return 1.10
+
+        case 'OusNormals':
+            return 1
+
+        case 'OusGrossos':
+            return 0.9
+    }
+}
+
+function aiguaCalc() {
+
+    let ousSubotal = ratioCalc(igAigua) * ousCalc()
+    return ousSubotal
+}
+
