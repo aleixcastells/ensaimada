d0d3cfdb72bc3106c18d5bab0e315094607090e5
aleixcastells <castells.94@gmail.com>
2023-01-29 18:52:59 +0100

    Code optimisation and cleanup
    Rewritten some functions to be more efficient and improve the clarity.
    
    Added some minor functions with the same purpose.
    
    Rearranged some functions so they appear in a more logical order.
    
    Minor tweaks to the recipe variables.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,12 @@
+console.log('Version 2.0')
 var settings = ['ensaïmada', '', '', '', '', '', '', '']
 var total_hints = document.getElementsByClassName('hint').length;
 console.log('Total hints: ', total_hints)
 
 // Buttons
-var btnArray = [
-    'tamany_normal', 'tamany_grossa', 'ous_petits', 'ous_normals',
-    'ous_grossos', 'ous_sense', 'vegana_on', 'vegana_off', 'temp_calor',
-    'temp_fred', 'xp_baix', 'xp_mig', 'xp_alt', 'lloc_mallorca',
-    'lloc_peninsula', 'lloc_guiri', 'de_llisa', 'de_cabell', 'de_xocolata'
-]
+var btnArray = ['tamany_normal', 'tamany_grossa', 'ous_petits', 'ous_normals', 'ous_grossos', 'ous_sense', 'vegana_on', 'vegana_off', 'temp_calor', 'temp_fred', 'xp_baix', 'xp_mig', 'xp_alt', 'lloc_mallorca', 'lloc_peninsula', 'lloc_guiri', 'de_llisa', 'de_cabell', 'de_xocolata']
 
 console.log('Total buttons: ', btnArray.length)
-
 var tamany_normal = document.getElementById('tamany_normal')
 var tamany_grossa = document.getElementById('tamany_grossa')
 var ous_petits = document.getElementById('ous_petits')
@@ -32,7 +27,7 @@ var de_llisa = document.getElementById('de_llisa')
 var de_cabell = document.getElementById('de_cabell')
 var de_xocolata = document.getElementById('de_xocolata')
 
-// Hints
+// Hints - - - - - - - - - - - - - - - - - - - 
 const hintList = []
 
 for (let i = 0; i < total_hints; i++) {
@@ -41,19 +36,16 @@ for (let i = 0; i < total_hints; i++) {
     let hintElement = document.getElementById(hintName)
     hintList.push(hintElement)
 }
-
 for (let i = 0; i < hintList.length; i++) {
     hintList[i].classList.add('hint-hidden')
 }
 
-// Ingredients
-var ingrMult = [0.7, 1, 1.3]
+// Ingredients - - - - - - - - - - - - - - - - - - - 
+var ingrMult = [0.7, 1, 1.3] // Size multiplication factor. 1 = Normal (default)
 var ingrMultSelect = 1
 var baseAigua = 100
-var extraAigua = 0
 
 const ingredients = {
-
     sucre: 150,
     ous: 2,
     farina: 500,
@@ -61,34 +53,42 @@ const ingredients = {
     llevadura: 15,
     temps_estiu: '\~ 10 h',
     temps_hivern: '\~ 18 h',
-
     sucreQ: function () { return this.sucre * ingrMult[ingrMultSelect] + ' g' },
     ousQ: function () { return Math.ceil(this.ous * ingrMult[ingrMultSelect]) + ' ous' },
     farinaQ: function () { return this.farina * ingrMult[ingrMultSelect] + ' g' },
-    aiguaQ: function () { return (this.aigua * ingrMult[ingrMultSelect] + extraAigua) + ' ml' },
+    aiguaQ: function () { return this.aigua * ingrMult[ingrMultSelect] + ' ml' },
     llevaduraQ: function () { return this.llevadura * ingrMult[ingrMultSelect] + ' g' },
 }
 
-document.getElementById('eggs').innerHTML = ingredients.ousQ()
-document.getElementById('water').innerHTML = ingredients.aiguaQ()
+function ingredientManager() {
+    document.getElementById('sucre').innerHTML = ingredients.sucreQ()
+    document.getElementById('ous').innerHTML = ingredients.ousQ()
+    document.getElementById('farina').innerHTML = ingredients.farinaQ()
+    document.getElementById('aigua').innerHTML = ingredients.aiguaQ()
+    document.getElementById('llevadura').innerHTML = ingredients.llevaduraQ()
+    document.getElementById('eggs').innerHTML = ingredients.ousQ()
+    document.getElementById('water').innerHTML = ingredients.aiguaQ()
 
+    settings[4] == 'temp_fred' ? document.getElementById("tempsPrep").innerHTML = ingredients.temps_hivern
+        : document.getElementById('tempsPrep').innerHTML = ingredients.temps_estiu
+}
+
+// UI Functionality - - - - - - - - - - - - - - - - - - - 
 function btnClick(n, text) {
-
+    console.clear()
     btnAction(n, text)
     updateUI(n, text)
+    console.log(settings)
 }
 
 function btnAction(n, text) {
-
     settings[n] = text
-
     if (n == 6 && text == 'lloc_guiri') {
         window.open('https://www.amazon.es/ensaimada-mallorquina/s?k=ensaimada+mallorquina', '_blank')
     }
 }
 
 function updateUI(n, text) {
-
     btnInteraction(n, text)
     hintManager()
     btnManager()
@@ -96,93 +96,89 @@ function updateUI(n, text) {
     btnInteraction(n, text)
 }
 
+function btnManager() {
+    for (let i = 0; i < btnArray.length; i++) {
+
+        let isActive = settings.indexOf(btnArray[i])
+        isActive == -1 ? flipBtn('btn-primary', 'btn-secondary', i)
+            : flipBtn('btn-secondary', 'btn-primary', i)
+    }
+}
+
+function flipBtn(typeOld, typeNew, id) {
+    document.getElementById(btnArray[id]).classList.replace(typeOld, typeNew)
+}
+
 function btnInteraction(n, text) {
-
     switch (n) {
-
         // Mida ensaïmada
         case 1:
             if (text == 'tamany_normal') {
                 ingrMultSelect = 1
-                extraAigua = 0
             }
             if (text == 'tamany_grossa') {
                 ingrMultSelect = 2
-
-                if (settings[3] == 'vegana_off') { extraAigua = 20 }
-                if (settings[3] == 'vegana_on') { extraAigua = 0 }
             }
+            break;
 
         // Ous: com són de grossos?
         case 2:
-            if (text == 'ous_petits') {
-                settings[3] = 'vegana_off'
-                document.getElementById(btnArray[6]).classList.replace('btn-success', 'btn-secondary')
-                ingredients.ous = 2;
-                ingredients.aigua = baseAigua + 5
-            }
-            if (text == 'ous_normals') {
-                settings[3] = 'vegana_off'
-                document.getElementById(btnArray[6]).classList.replace('btn-success', 'btn-secondary')
-                ingredients.ous = 2;
-                ingredients.aigua = baseAigua
-            }
-            if (text == 'ous_grossos') {
-                settings[3] = 'vegana_off'
-                document.getElementById(btnArray[6]).classList.replace('btn-success', 'btn-secondary')
-                ingredients.ous = 2;
-                ingredients.aigua = baseAigua - 5
-            }
+
             if (text == 'ous_sense') {
                 ingredients.ous = 0;
                 ingredients.aigua = 240
+            }
+            else {
+                settings[3] = 'vegana_off'
+                flipBtn('btn-success', 'btn-secondary', 6)
+                ingredients.ous = 2;
+                text == 'ous_petits' ? ingredients.aigua = baseAigua + 10
+                    : text == 'ous_grossos' ? ingredients.aigua = baseAigua - 10
+                        : ingredients.aigua = baseAigua
             }
             break;
 
         // Vegana?
         case 3:
             if (text == 'vegana_on') {
-                document.getElementById(btnArray[6]).classList.replace('btn-primary', 'btn-success')
+                flipBtn('btn-primary', 'btn-success', 6)
                 settings[2] = 'ous_sense'
                 ingredients.ous = 0
                 ingredients.aigua = 240
-                extraAigua = 0
 
             }
             if (text == 'vegana_off') {
-                document.getElementById(btnArray[6]).classList.replace('btn-success', 'btn-secondary')
+                flipBtn('btn-success', 'btn-secondary', 6)
                 settings[2] = 'ous_normals'
                 ingredients.ous = 2;
                 ingredients.aigua = baseAigua
-                if (settings[1] == 'tamany_grossa') { extraAigua = 20 }
-                if (settings[1] == 'tamany_normal') { extraAigua = 0 }
             }
             break;
 
         // Nivell d'experiència
         case 5:
-            if (text == 'xp_baix') { }
-            if (text == 'xp_mig') { }
-            if (text == 'xp_alt') {
-                document.getElementById(btnArray[12]).classList.replace('btn-primary', 'btn-danger')
-            }
-            else {
-                document.getElementById(btnArray[12]).classList.replace('btn-danger', 'btn-secondary')
-            }
-            llisaMillor()
-            break;
+            text == 'xp_alt' ? flipBtn('btn-primary', 'btn-danger', 12)
+                : flipBtn('btn-danger', 'btn-secondary', 12)
 
-        // D'on ets?
-        case 6:
-            if (text == 'lloc_mallorca') { }
-            if (text == 'lloc_peninsula') { }
-            if (text == 'lloc_guiri') { }
+            llisaMillor()
             break;
 
         //De que la vols?
         case 7:
             llisaMillor()
             break;
+    }
+}
+
+function hintManager() {
+
+    let getClassName = ''
+    for (let i = 0; i < hintList.length; i++) {
+        getClassName = hintList[i].className
+        classNameArray = getClassName.split(' ')
+        let arrayCheck = settings.some(item => classNameArray.includes(item))
+        arrayCheck == true ? hintList[i].classList.replace('hint-hidden', 'hint-visible')
+            : hintList[i].classList.replace('hint-visible', 'hint-hidden')
     }
 }
 
@@ -194,63 +190,9 @@ function llisaMillor() {
     }
 }
 
-function hintManager() {
-
-    let getClassName = ''
-
-    for (let i = 0; i < hintList.length; i++) {
-
-        getClassName = hintList[i].className
-        classNameArray = getClassName.split(' ')
-        let arrayCheck = settings.some(item => classNameArray.includes(item))
-
-        if (arrayCheck == true) { hintList[i].classList.replace('hint-hidden', 'hint-visible') }
-        else { hintList[i].classList.replace('hint-visible', 'hint-hidden') }
-    }
-}
-
-function btnManager() {
-
-    for (let i = 0; i < btnArray.length; i++) {
-
-        let isActive = settings.indexOf(btnArray[i])
-
-        if (isActive == -1) { document.getElementById(btnArray[i]).classList.replace('btn-primary', 'btn-secondary') }
-        else { document.getElementById(btnArray[i]).classList.replace('btn-secondary', 'btn-primary') }
-    }
-}
-
-function ingredientManager() {
-    document.getElementById('sucre').innerHTML = ingredients.sucreQ()
-    document.getElementById('ous').innerHTML = ingredients.ousQ()
-    document.getElementById('farina').innerHTML = ingredients.farinaQ()
-    document.getElementById('aigua').innerHTML = ingredients.aiguaQ()
-    document.getElementById('llevadura').innerHTML = ingredients.llevaduraQ()
-
-    if (settings[4] == 'temp_fred') {
-        document.getElementById("tempsPrep").innerHTML = ingredients.temps_hivern
-    }
-    if (settings[4] == 'temp_calor') {
-        document.getElementById('tempsPrep').innerHTML = ingredients.temps_estiu
-    }
-
-    document.getElementById('eggs').innerHTML = ingredients.ousQ()
-    document.getElementById('water').innerHTML = ingredients.aiguaQ()
-}
-
 function show(h = 'hint') {
 
-    switch (h) {
-
-        case 'hint':
-            settings[0] = 'hint'
-            break;
-
-        case 'hide':
-            settings[0] = ''
-            break;
-    }
-
+    h == 'hint' ? settings[0] = 'hint' : settings[0] = 'ensaïmada'
     hintManager()
     return settings
 }
